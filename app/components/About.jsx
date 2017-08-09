82347fa440c97d904da4c7ddc8494da56cd16553
Allison Alexander <aveleber@hotmailcom>
2017-08-09 14:15:58 -0400

    started to separate skills from about section

--- app/components/About.jsx
+++ app/components/About.jsx
@@ -27,35 +27,16 @@ export default () => {
                 and I am excited to return to the field
                 as a full stack developer!
           </div>
+          <br/>
+          <div className="flex-item-col font">
+            <a href="https://docs.google.com/document/d/1kjKBbR1lA3fPZljm9jUDXntesqhqrSQJqk9fyKhzN-w/edit?usp=sharing"
+               target="_blank">
+            Please view my resume!
+            </a>
+          </div>
       </div>
       <div className="well flex-item-about">
         <img src="allison in hoboken.jpg"/>
-      </div>
-      <div className="flex-item-about">
-        <h3 className="well center-text font skills">Skills</h3>
-        <div className="flex-container-home">
-          <div className="flex-item-skills">JavaScript</div>
-          <div className="flex-item-skills">Node.js</div>
-          <div className="flex-item-skills">Express.js</div>
-          <div className="flex-item-skills">Java</div>
-          <div className="flex-item-skills">Sequelize</div>
-          <div className="flex-item-skills">PostgreSQL</div>
-          <div className="flex-item-skills">SQL</div>
-          <div className="flex-item-skills">React.js</div>
-          <div className="flex-item-skills">Redux</div>
-          <div className="flex-item-skills">React-Redux</div>
-          <div className="flex-item-skills">Firebase</div>
-          <div className="flex-item-skills">HTML</div>
-          <div className="flex-item-skills">CSS</div>
-          <div className="flex-item-skills">Bootstrap</div>
-          <div className="flex-item-skills">Flexbox</div>
-          <div className="flex-item-skills">Agile</div>
-          <div className="flex-item-skills">Estimation</div>
-          <div className="flex-item-skills">Debugging</div>
-          <div className="flex-item-skills">Unit Testing</div>
-          <div className="flex-item-skills">Integration Testing</div>
-          <div className="flex-item-skills">Production Support</div>
-        </div>
       </div>
     </div>
   )
