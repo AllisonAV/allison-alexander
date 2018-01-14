d0411d2d0392ba42b7b4816f3d209c59cfa3d130
Allison Alexander <aveleber@hotmailcom>
2018-01-13 21:59:42 -0500

    updated skills and about

--- app/components/Skills.jsx
+++ app/components/Skills.jsx
@@ -2,20 +2,26 @@ import React from 'react'
 export default () => {
   return (
     <div className="flex-container-skills">
-      <div className="flex-item-about">
-      <h3 className="well center-text font skills">Skills</h3>
+      <div className="container-skills">
+        <h3 className="well center-text font skills">Skills</h3>
         <div className="flex-container-home">
+          <div className="flex-item-skills">PHP 7</div>
+          <div className="flex-item-skills">PHPUnit</div>
+          <div className="flex-item-skills">PostgreSQL</div>
+          <div className="flex-item-skills">SQL</div>
           <div className="flex-item-skills">JavaScript</div>
           <div className="flex-item-skills">Node.js</div>
           <div className="flex-item-skills">Express.js</div>
           <div className="flex-item-skills">Java</div>
           <div className="flex-item-skills">Sequelize</div>
-          <div className="flex-item-skills">PostgreSQL</div>
-          <div className="flex-item-skills">SQL</div>
           <div className="flex-item-skills">React.js</div>
           <div className="flex-item-skills">Redux</div>
           <div className="flex-item-skills">React-Redux</div>
           <div className="flex-item-skills">Firebase</div>
+          <div className="flex-item-skills">RabbitMQ</div>
+          <div className="flex-item-skills">Docker</div>
+          <div className="flex-item-skills">Marathon</div>
+          <div className="flex-item-skills">Kibana</div>
           <div className="flex-item-skills">HTML</div>
           <div className="flex-item-skills">CSS</div>
           <div className="flex-item-skills">Bootstrap</div>
@@ -23,9 +29,9 @@ export default () => {
           <div className="flex-item-skills">Agile</div>
           <div className="flex-item-skills">Estimation</div>
           <div className="flex-item-skills">Debugging</div>
-          <div className="flex-item-skills">Unit Testing</div>
-          <div className="flex-item-skills">Integration Testing</div>
-          <div className="flex-item-skills">Production Support</div>
+          <div className="flex-item-skills-bottom">Unit Testing</div>
+          <div className="flex-item-skills-bottom">Integration Testing</div>
+          <div className="flex-item-skills-bottom">Production Support</div>
         </div>
       </div>
     </div>
